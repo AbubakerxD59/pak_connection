88eb5d4d4b99247f7e847a8e9803c0c852db1eff
Muhammad Abubaker <abmasood5900@gmail.com>
2025-03-27 02:17:06 +0500

    minor changes

--- database/migrations/2025_03_26_121232_create_webhook_calls_table.php
+++ database/migrations/2025_03_26_121232_create_webhook_calls_table.php
@@ -1,23 +1,32 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up()
     {
         Schema::create('webhook_calls', function (Blueprint $table) {
-            $table->bigIncrements('id');
-
+            $table->id();
             $table->string('name');
             $table->string('url');
-            $table->json('headers')->nullable();
-            $table->json('payload')->nullable();
+            $table->longText('headers')->nullable();
+            $table->longText('payload')->nullable();
             $table->text('exception')->nullable();
-
             $table->timestamps();
         });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('webhook_calls');
     }
 };
