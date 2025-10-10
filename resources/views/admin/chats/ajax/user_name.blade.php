c556ef4aca0948782d81b9c88b9f5b96f6fcccac
Muhammad Abubaker <abmasood5900@gmail.com>
2025-10-10 20:19:06 +0500

    minor changes

--- resources/views/admin/chats/ajax/user_name.blade.php
+++ resources/views/admin/chats/ajax/user_name.blade.php
@@ -1,7 +1,5 @@
 <div>
-    <span>{{ @$user->full_name }}</span>
-    <br>
+    <span>{{ @$user->full_name . ' (' . @$user->membership_id . ')' }}</span>
     <span>{{ @$user->email }}</span>
     <br>
-    <span>{{ @$user->membership_id }}</span>
 </div>
