e80bbc027832f620226c723119e102a287d182b7
Muhammad Abubaker <abmasood5900@gmail.com>
2025-10-10 20:26:16 +0500

    minor changes

--- resources/views/admin/chats/ajax/user_name.blade.php
+++ resources/views/admin/chats/ajax/user_name.blade.php
@@ -1,5 +1,9 @@
 <div>
-    <span>{{ @$user->full_name . ' (' . @$user->membership_id . ')' }}</span>
+    <span>
+        <a href="{{ route('users.edit', @$user->id) }}">
+            {{ @$user->full_name . ' (' . @$user->membership_id . ')' }}
+        </a>
+    </span>
     <br>
     <span>{{ @$user->email }}</span>
 </div>
